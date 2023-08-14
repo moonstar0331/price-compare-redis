83beaa50c54da0fa9f03588248439973011aeb5a
MoonStar <moonsung0331@gmail.com>
2023-08-14 16:18:53 +0900

    feat: redis 관련 code 작성 및 배포 - 2

--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,16 +1,22 @@
 package com.example.pricecompareredis.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class LowestPriceServiceImpl implements LowestPriceService {
+
+    private final RedisTemplate myProdPriceRedis;
 
     @Override
     public Set getZsetValue(String key) {
         Set myTempSet = new HashSet();
+        myTempSet = myProdPriceRedis.opsForZSet().rangeByScoreWithScores(key, 0, 9);
         return myTempSet;
     }
 }
