9b4e8d9ced512af99d4abacbc8efdf7d89a8ddd9
MoonStar <moonsung0331@gmail.com>
2023-08-17 17:16:06 +0900

    feat: redis 관련 code 작성 및 배포 - 4

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -1,7 +1,9 @@
 package com.example.pricecompareredis.controller;
 
 import com.example.pricecompareredis.service.LowestPriceService;
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,5 +24,15 @@ public class LowestPriceController {
     @PutMapping("/product")
     public int setNewProduct(@RequestBody Product newProduct) {
         return lowestPriceService.setNewProduct(newProduct);
+    }
+
+    @PutMapping("/productGroup")
+    public int setNewProduct(@RequestBody ProductGrp newProductGrp) {
+        return lowestPriceService.setNewProductGrp(newProductGrp);
+    }
+
+    @PutMapping("/productGroupToKeyword")
+    public int setNewProductGrpToKeyword(String keyword, String proGrpId, double score) {
+        return lowestPriceService.setNewProductGrpToKeyword(keyword, proGrpId, score);
     }
 }
--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.pricecompareredis.service;
 
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -26,6 +28,23 @@ public class LowestPriceServiceImpl implements LowestPriceService {
         int rank;
         myProdPriceRedis.opsForZSet().add(newProduct.getProductGrpId(), newProduct.getProductId(), newProduct.getPrice());
         rank = myProdPriceRedis.opsForZSet().rank(newProduct.getProductGrpId(), newProduct.getProductId()).intValue();
+        return rank;
+    }
+
+    @Override
+    public int setNewProductGrp(ProductGrp newProductGrp) {
+        List<Product> product = newProductGrp.getProductList();
+        String productId = product.get(0).getProductId();
+        int price = product.get(0).getPrice();
+        myProdPriceRedis.opsForZSet().add(newProductGrp.getProductGrpId(), productId, price);
+        int productCnt = myProdPriceRedis.opsForZSet().zCard(newProductGrp.getProductGrpId()).intValue();
+        return productCnt;
+    }
+
+    @Override
+    public int setNewProductGrpToKeyword(String keyword, String proGrpId, double score) {
+        myProdPriceRedis.opsForZSet().add(keyword, proGrpId, score);
+        int rank = myProdPriceRedis.opsForZSet().rank(keyword, proGrpId).intValue();
         return rank;
     }
 }
